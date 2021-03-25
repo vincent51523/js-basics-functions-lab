de286071c589ee71c5d887013d528bc0db2db8e7
bin <vincent418@icloud.com>
2021-03-25 01:35:01 -0500

    commit tree b385a33d784bd4b9aa4273b866e576446cd329fe

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function distanceFromHqInBlocks(street) {
     return Math.abs( street - 42)
 } 
-function distanceFromHqInFeet(street)
-    return 
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
+}
+function distanceTravelledInFeet(x, y) {
+    return Math.abs(y - x) * 264
+}
+function calculatesFarePrice(start, destination) {
+    let flatfair = 25
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet < 400) {
+    return 0 
+    } else if (2000 < feet && feet< 2500) {
+    return flatfair
+    } else if (400 < feet && feet < 2000){
+        return (feet - 400)* 0.02
+    } else if (feet > 2000 && feet < 2500) {
+        return 25
+    }
+    else { 
+        return 'cannot travel that far'
+    }
+    
+    
+}
